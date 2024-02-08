2f37f59d8c60f8ffa05f76389e79381031145986
Leonie10 <leonie.10@hotmail.fr>
2024-02-08 16:27:07 +0100

    pull changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,7 +7,7 @@ import './App.css'
 import HomePage from "./pages/Home"
 import AuthenticationPage from './pages/Authentication'
 import RootLayoutPage from "./pages/RootLayout"
-import SignUpPage from "./pages/SignUp";
+
 
 function App() {
 
@@ -17,7 +17,6 @@ function App() {
       children: [
         {path: '/', element: <HomePage/> }, 
         {path: '/auth', element: <AuthenticationPage/>},
-        {path: '/signup', element: <SignUpPage/>}
       ]
     }
     
